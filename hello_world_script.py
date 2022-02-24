a27e9edb561794397e538ef3d075f5cbf7106586
Ronaldo e Silva Vieira <ronaldo.vieira@dcc.ufmg.br>
2022-02-24 07:49:19 -0300

    Add hello world for script env

---
+++ hello_world_script.py
@@ -0,0 +1,24 @@
+import numpy as np
+import time
+
+from gym_microrts import microrts_ai
+from gym_microrts.envs.new_vec_env import MicroRTSScriptVecEnv
+
+env = MicroRTSScriptVecEnv(
+    ai2s=[microrts_ai.coacAI],
+    max_steps=2000,
+    render_theme=2,
+    map_path="maps/16x16/basesWorkers16x16.xml",
+    reward_weight=np.array([10.0, 1.0, 1.0, 4.0, 4.0, 4.0, 0.2, 0.2, 1.0])
+)
+
+env.reset()
+
+for i in range(10000):
+    env.render()
+    time.sleep(0.01)
+
+    action = [env.action_space.sample()]
+    next_obs, reward, done, info = env.step(action)
+
+env.close()
