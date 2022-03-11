561cb0ef1e21a84f0ef26e654dc0692eb9b9c17e
Ronaldo e Silva Vieira <ronaldo.vieira@dcc.ufmg.br>
2022-03-11 12:17:27 -0300

    Fix scripts histogram
    BuildBarracks was not showing

--- experiments/new_ppo_scripts.py
+++ experiments/new_ppo_scripts.py
@@ -380,7 +380,7 @@ if __name__ == "__main__":
                     for key in info["microrts_stats"]:
                         run.log({f"rewards/{key}": info["microrts_stats"][key]}, step=global_step)
 
-                    hist, _ = np.histogram(actions.cpu().flatten(), range(0, 6))
+                    hist, _ = np.histogram(actions.cpu().flatten(), range(0, 7))
                     hist = hist / sum(hist)
                     scripts = ["workerRush", "lightRush", "rangedRush", "heavyRush", "expand", "buildBarracks"]
 
